d12d88bd190e35db58c956ff2e52204fa8b47441
OisinLoftus-STUDENT <G00470518@atu.ie>
2026-03-04 17:22:29 +0000

    Created Booking Service Layer with methods for adding a booking and retriving by id as well as getting all bookings.

--- src/main/java/ie/atu/oop_inclass_07/service/BookingService.java
+++ src/main/java/ie/atu/oop_inclass_07/service/BookingService.java
@@ -1,13 +1,40 @@
 package ie.atu.oop_inclass_07.service;
 
 
+import ie.atu.oop_inclass_07.model.Booking;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BookingService {
 
-    final private BookingService bookingService = ArrayList
+    final private ArrayList<Booking> bookings = new ArrayList<>();
+
+    private long nextId = 1;
+
+
+    public Booking addBooking(Booking booking) {
+        booking.setBookingId(nextId++);
+        bookings.add(booking);
+        return booking;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public Booking getBookingById(long id) {
+        Booking selectedBooking = null;
+
+        for (Booking booking : bookings) {
+            if (booking.getBookingId() == id) {
+                selectedBooking = booking;
+            }
+        }
+        return selectedBooking;
+    }
+
 
 }
